abf27a4040e88221d63deda95c71a083ca17b649
DuyLam <dl.duylam.2000@gmail.com>
2023-03-13 16:45:54 +0700

    feat: update Mar 03 2023 - 16:45 - DuyLam

--- src/v1/chat/controller.js
+++ src/v1/chat/controller.js
@@ -3,11 +3,14 @@ import { Configuration, OpenAIApi } from "openai";
 export async function chat(req, res) {
   const { question } = req.body;
 
+  const rules = ["sex", "18+"];
+  question.split(" ").forEach((word) => {
+    if (rules.includes(word)) throw new Error("Yêu cầu chứa từ ngữ vi phạm");
+  });
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: question }],
