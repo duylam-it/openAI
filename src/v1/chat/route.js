14c94f06c520ccd5882d28957d6b92f018b9cef4
DuyLam <dl.duylam.2000@gmail.com>
2023-03-14 08:38:42 +0700

    feat: update Mar 14 2023 - 08:38 - DuyLam

--- src/v1/chat/route.js
+++ src/v1/chat/route.js
@@ -1,8 +1,7 @@
 import express from "express";
-import { catchErrors } from "../handlers/errorHandlers.js";
 import { chat } from "./controller.js";
 const router = express.Router();
 
-router.post("/", catchErrors(chat));
+router.post("/", chat);
 
 export default router;
