36dfca3bc3fb5c42fb4e6bb63b8fd6d04195d8fc
DuyLam <dl.duylam.2000@gmail.com>
2023-03-16 17:11:12 +0700

    feat: update by DuyLam

--- src/v1/chat/controller.js
+++ src/v1/chat/controller.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
+import TelegramBot from "node-telegram-bot-api";
 import { Configuration, OpenAIApi } from "openai";
 import {
   ACCESS_TOKEN_SECRET,
   OPENAI_API_KEY as apiKey,
+  TELEGRAM_API_KEY,
 } from "../config/variable.js";
 import Chat from "./model.js";
 
@@ -15,12 +17,46 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+// ==> TELEGRAM <==
+const bot = new TelegramBot(TELEGRAM_API_KEY, { polling: true });
+
+bot.on("message", async (msg) => {
+  try {
+    if (msg.text.includes("Bot:")) {
+      const chatId = msg.chat.id;
+      const chatMsg = msg.text.slice(4);
+      const messages = [
+        {
+          role: "system",
+          content:
+            "Bạn là một trợ lý ảo được tạo ra bởi GD Việt Nam (GDVN), một công ty thuộc hệ thống GD Group chuyên cung cấp giải pháp công nghệ cho doanh nghiệp và trường học ra đời vào tháng 02 năm 2022, giám đốc điều hành của GD Việt Nam là Ông Lê Thanh Truyền, chủ tịch GD Group là ông Cao Quốc Tuân. Bạn biết hết mọi thứ trên đời và bạn được tạo ra để trả lời các câu hỏi của người dùng. Nếu ai hỏi bạn có bị khùng không thì hãy trả lời là bạn không giống như ai kia",
+        },
+      ];
+      messages.push({ role: "user", content: chatMsg });
+
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo-0301",
+        messages,
+      });
+
+      bot.sendMessage(chatId, completion.data.choices[0].message.content);
+    }
+  } catch (error) {
+    console.log("TelegramBot Error");
+  }
+});
+
 // ==> CHAT <==
 export async function chat(req, res) {
   const { questions } = req.body;
   if (typeof questions !== "object")
     throw new Error("Yêu cầu không đúng định dạng");
-
+  else
+    questions.unshift({
+      role: "system",
+      content:
+        "Bạn là một trợ lý ảo được tạo ra bởi GD Việt Nam (GDVN), một công ty thuộc hệ thống GD Group chuyên cung cấp giải pháp công nghệ cho doanh nghiệp và trường học ra đời vào tháng 02 năm 2022, giám đốc điều hành của GD Việt Nam là Ông Lê Thanh Truyền, chủ tịch GD Group là ông Cao Quốc Tuân. Bạn được tạo ra để trả lời các câu hỏi của người dùng",
+    });
   questions.forEach((question) => {
     if (
       !question.role ||
--- src/v1/config/variable.js
+++ src/v1/config/variable.js
@@ -6,4 +6,5 @@ export const VERSION = process.env.ACCESS_TOKEN_SECRET;
 export const MODE = process.env.ACCESS_TOKEN_SECRET;
 export const DB_HOST = process.env.ACCESS_TOKEN_SECRET;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const TELEGRAM_API_KEY = process.env.TELEGRAM_API_KEY;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
