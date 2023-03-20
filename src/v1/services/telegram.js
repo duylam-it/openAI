e613486a54bcaf05bd47493b2d57cace9ce7cbcc
duylam-it <73417194+duylam-it@users.noreply.github.com>
2023-03-20 23:43:00 +0700

    Update telegram.js

--- src/v1/services/telegram.js
+++ src/v1/services/telegram.js
@@ -34,6 +34,7 @@ const connect = () => {
       let content = "";
       if(ctx.update.message.text.slice(3).trim() === "") content = "Xin chào";
       else content = ctx.update.message.text.slice(3).trim();
+      if(temp.indexOf({role: "user", content}) !== -1) temp.splice(temp.indexOf({role: "user", content}), 1);
       temp.push({
         role: "user",
         content,
